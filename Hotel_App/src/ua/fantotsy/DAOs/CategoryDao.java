77d561520aa78c11572c697cfb7388a85e79e08f
fantotsy <fantotsy@gmail.com>
2016-08-23 13:02:24 +0300

    Stored_Procedure_Added

--- Hotel_App/src/ua/fantotsy/DAOs/CategoryDao.java
+++ Hotel_App/src/ua/fantotsy/DAOs/CategoryDao.java
@@ -5,10 +5,7 @@ import ua.fantotsy.datasource.ConnectionPool;
 import ua.fantotsy.entities.Category;
 import ua.fantotsy.utils.SqlQueriesGetter;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,25 +39,17 @@ public class CategoryDao implements ICategoryDao {
     public List<Category> getAllCategoriesForUser(String arrival, String departure, List<String> types, List<String> capacities) {
         List<Category> listOfCategories = new ArrayList<>();
         try (Connection connection = ConnectionPool.getInstance().getConnection();
-             PreparedStatement ps = connection.prepareStatement(SqlQueriesGetter.getInstance().getSQLQuery(SqlQueriesGetter.GET_ALL_AVAILABLE_APARTMENTS_FOR_GUEST))) {
-            ps.setString(1, arrival);
-            ps.setString(2, arrival);
-            ps.setString(3, departure);
-            ps.setString(4, departure);
-            ps.setString(5, arrival);
-            ps.setString(6, departure);
-            ResultSet rs;
-            for (String type : types) {
-                for (String capacity : capacities) {
-                    ps.setString(7, type);
-                    ps.setString(8, capacity);
-                    rs = ps.executeQuery();
-                    while (rs.next()) {
-                        Category category = new Category(rs.getInt("category_id"), rs.getString("type"), rs.getInt("number_of_beds"), rs.getInt("price"), rs.getInt("apartments"));
-                        listOfCategories.add(category);
-                    }
-                }
+             CallableStatement cs = connection.prepareCall("{call get_all_categories_for_user(?, ?, ?, ?)}")) {
+            cs.setString(1, arrival);
+            cs.setString(2, departure);
+            cs.setString(3, types.toString());
+            cs.setString(4, capacities.toString());
+            ResultSet rs = cs.executeQuery();
+            while (rs.next()) {
+                Category category = new Category(rs.getInt("category_id"), rs.getString("type"), rs.getInt("number_of_beds"), rs.getInt("price"), rs.getInt("apartments"));
+                listOfCategories.add(category);
             }
+            rs.close();
         } catch (SQLException e) {
             Logger logger = Logger.getLogger(CategoryDao.class.getName());
             logger.error(e);
